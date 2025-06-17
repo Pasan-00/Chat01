2f8edcc4751572343e9e4520cbc0789b8d1550b8
Pasan Thilakshana <pasanudara999@gmail.com>
2025-06-17 03:38:53 -0700

    Implement PDF loading, text splitting, and embedding functionality; add chat interface and styling

---
+++ src/prompt.py
@@ -0,0 +1,11 @@
+
+
+
+system_prompt = (
+    "You are an assistant fot question-answering tasks. "
+    "If you don't know the answer, just say you don't know. "
+    "Answer concisely."
+    "\n\n"
+    "{context}"
+    
+)
---
+++ store_index.py
@@ -0,0 +1,34 @@
+from src.helper import load_pdf_file, text_split, download_hugging_face_embeddings
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec 
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+
+extracted_data = load_pdf_file(data='data/')
+text_chunks = text_split(extracted_data)
+embeddings = download_hugging_face_embeddings()
+
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "testv2"
+
+
+# Only create the index if it does not already exist
+if index_name not in pc.list_indexes().names():
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        )
+    )
+else:
+    print(f"Index '{index_name}' already exists.")
